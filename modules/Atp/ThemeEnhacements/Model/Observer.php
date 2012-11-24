9c1b88f61947bb263e32e6edd4f02e63495f8bdc
Mauricio <self@maurousuga.me>
2012-11-24 01:00:32 +0000

    Store's layout update was breaking checkout. Fixed.

--- modules/Atp/ThemeEnhacements/Model/Observer.php
+++ modules/Atp/ThemeEnhacements/Model/Observer.php
@@ -7,8 +7,16 @@ class Atp_ThemeEnhacements_Model_Observer
     $store = Mage::app()->getStore()->getCode();
     $action = $observer->getEvent()->getAction()->getFullActionName();
     $update = 'STORE_' . $store . '_' . strtolower($action);
-    //xdebug_break();
     $observer->getEvent()->getLayout()->getUpdate()->addHandle($update);
+  }
+
+  public function appendStoreClass(Varien_Event_Observer $observer) {
+    $store = Mage::app()->getStore()->getCode();
+    $root = $observer->getEvent()->getLayout()->getBlock('root');
+    if ($root && method_exists($root, 'addBodyClass')) {
+      //xdebug_break();
+      $root->addBodyClass('store-' . $store);
+    }
   }
 
 }
