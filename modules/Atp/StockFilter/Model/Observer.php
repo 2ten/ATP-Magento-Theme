a84e4882d6586a2024cdbfafe8f141c324f9ed4b
Mauricio <self@maurousuga.me>
2013-01-28 15:26:09 +0000

    StockFilter module and some cleanup.

--- modules/Atp/StockFilter/Model/Observer.php
+++ modules/Atp/StockFilter/Model/Observer.php
@@ -1,16 +1,21 @@
 <?php
 
-class Atp_StockFilter_Model_Observer {
+class Atp_StockFilter_Model_Observer
+{
 
-  public static function checkStockAndDisable($observer) {
-    $storeId = 0;
+  const STORE_ADMIN = 0;
+  const STORE_JEWELRY = 1;
+  const STORE_CERAMICS = 2;
+
+  public function checkStockAndDisable($observer) {
     $order = $observer->getEvent()->getOrder();
-    xdebug_break();
     foreach ($order->getItemsCollection() as $item) {
+      $storeId = strtolower(substr($item->getSku(), 0, 3)) == 'cer' ? self::STORE_CERAMICS : self::STORE_ADMIN;
       $stockQty = (int)Mage::getModel('cataloginventory/stock_item')->loadByProduct($item->getProductId())->getQty();
       if ($stockQty == 0) {
         Mage::getModel('catalog/product_status')->updateProductStatus($item->getProductId(), $storeId, Mage_Catalog_Model_Product_Status::STATUS_DISABLED);
       }
     }
   }
+
 }
