ca2cc7fde560ecbd89d15690fdc2d2008a717e2a
Mauricio <self@maurousuga.me>
2013-01-29 04:46:01 +0000

    Nope. When products get 0 stock they don't need to be disabled, just their state changed to out-of-stock

--- modules/Atp/StockFilter/Model/Observer.php
+++ modules/Atp/StockFilter/Model/Observer.php
@@ -2,18 +2,15 @@
 
 class Atp_StockFilter_Model_Observer
 {
-
-  const STORE_ADMIN = 0;
-  const STORE_JEWELRY = 1;
-  const STORE_CERAMICS = 2;
-
-  public function checkStockAndDisable($observer) {
+  public function checkStock($observer) {
     $order = $observer->getEvent()->getOrder();
+    $storeId = 0;
     foreach ($order->getItemsCollection() as $item) {
-      $storeId = strtolower(substr($item->getSku(), 0, 3)) == 'cer' ? self::STORE_CERAMICS : self::STORE_ADMIN;
-      $stockQty = (int)Mage::getModel('cataloginventory/stock_item')->loadByProduct($item->getProductId())->getQty();
-      if ($stockQty == 0) {
-        Mage::getModel('catalog/product_status')->updateProductStatus($item->getProductId(), $storeId, Mage_Catalog_Model_Product_Status::STATUS_DISABLED);
+      $stock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($item->getProductId());
+      $qty = (int)$stock->getQty();
+      if ($qty == 0) {
+        $stock->setData('is_in_stock', 0);
+        $stock->save();
       }
     }
   }
